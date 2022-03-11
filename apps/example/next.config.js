4e45b7aa596a12781627fa0a4e183c57b5210cba
Nick Sinclair <nick@parkable.com>
2022-03-11 14:45:30 +1300

    Get rest working

--- apps/example/next.config.js
+++ apps/example/next.config.js
@@ -1,9 +1,3 @@
-const withTM = require('next-transpile-modules')([
-  '@brail/mjml',
-  '@brail/core',
-  '@brail/react',
-]); // pass the modules you would like to see transpiled
-
 const nextConfig = {};
 
-module.exports = withTM(nextConfig);
+module.exports = nextConfig;
--- apps/example/pages/notification-template/index.tsx
+++ apps/example/pages/notification-template/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { TemplatePage } from '@brail/react';
 
 export { getStaticProps } from './index.template';
