c23b26f4c070af4d72fc400f9bd8c9a74f70d173
Nick Sinclair <nicksinclair@xtra.co.nz>
2022-10-26 13:16:55 +1300

    Fix issue with image case-sensitivity

--- packages/docs/src/pages/index.tsx
+++ packages/docs/src/pages/index.tsx
@@ -12,7 +12,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <img src="/img/Logo.svg" width={200} height="auto" />
+        <img src="/img/brail-logo.svg" width={200} height="auto" />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
