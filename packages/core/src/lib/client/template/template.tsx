64d637a46e45c0aa39295fa48218f805d22cb275
Nick Sinclair <nicksinclair@xtra.co.nz>
2022-10-28 15:24:38 +1300

    Remove unnecessary dep

--- packages/core/src/lib/client/template/template.tsx
+++ packages/core/src/lib/client/template/template.tsx
@@ -6,7 +6,6 @@ import {
   TemplateMethods,
 } from '../../types/template.types';
 import 'reflect-metadata';
-import HtmlReactParser from 'html-react-parser';
 
 const DEFAULT_MJML_OPTIONS: MjType.Mjml2HtmlOptions = {
   beautify: true,
@@ -28,7 +27,7 @@ export function createTemplate<P extends { [key: string]: any } = any>(
       defaultOptions
     );
 
-    return HtmlReactParser(html) as JSX.Element;
+    return <div dangerouslySetInnerHTML={{ __html: html }} />;
   };
 
   const methods: TemplateMethods<P> = {
