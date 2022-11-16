24b0d41467362a4ad69d969a4ee3a4104a3003c7
Nick Sinclair <nicksinclair@xtra.co.nz>
2022-11-16 14:27:33 +1300

    Add more examples, for edge cases

--- apps/example/pages/api/[...path].ts
+++ apps/example/pages/api/[...path].ts
@@ -1,11 +1,18 @@
 import { createServer, getBrailApiConfig } from '@brail/core/server';
+import EmptyTemplate from '../empty/index.template';
 import NestedWelcomeTemplate from '../nested/welcome/index.template';
+import NoPropsTemplate from '../no-props/index.template';
 import WelcomeTemplate from '../welcome/index.template';
 
 // Optional: Provide Nextjs API Route configuration to silence unneeded warnings
 export const config = getBrailApiConfig();
 
 // Register the email templates, so they can be served via Next API
-const server = createServer([WelcomeTemplate, NestedWelcomeTemplate]);
+const server = createServer([
+  WelcomeTemplate,
+  NestedWelcomeTemplate,
+  EmptyTemplate,
+  NoPropsTemplate,
+]);
 
 export default server;
