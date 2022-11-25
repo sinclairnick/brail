04588dcbbe142b31820c2eedd706986872897474
Michael Novotny <manovotny@gmail.com>
2022-11-24 19:20:14 -0600

    Lowercase everything.

--- packages/docs/docusaurus.config.js
+++ packages/docs/docusaurus.config.js
@@ -63,7 +63,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'Introduction/intro',
+            docId: 'introduction/intro',
             position: 'left',
             label: 'Tutorial',
           },
