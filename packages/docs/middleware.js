a5d298bc71e7ae79c91d784a1550f2e34f7c5696
Michael Novotny <manovotny@gmail.com>
2022-11-24 19:16:08 -0600

    Remove middleware.

--- packages/docs/middleware.js
+++
@@ -1,7 +0,0 @@
-export default function middleware(request) {
-  if (request.url.match(/\/[A-Z]/)) {
-    const url = new URL(request.url);
-    url.pathname = url.pathname.toLowerCase();
-    return Response.redirect(url);
-  }
-}
