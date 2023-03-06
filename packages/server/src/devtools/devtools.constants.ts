fe29adc61a7dcc66aef66930a991cbe776c02931
Nick Sinclair <nicksinclair@xtra.co.nz>
2023-03-07 11:16:43 +1300

    Default, disable devtools in prod

--- packages/server/src/devtools/devtools.constants.ts
+++ packages/server/src/devtools/devtools.constants.ts
@@ -1,9 +1,12 @@
 import { DevtoolsConfig, InitDevtoolsArgs } from "./devtools.types";
 
+const isProd = process.env.NODE_ENV === "production";
+const isDev = process.env.NODE_ENV === "development";
+
 export const getDevtoolsConfig = (args: InitDevtoolsArgs): DevtoolsConfig => {
   return {
     apiPath: args.apiPath ?? "/api/devtools",
-    isEnabled: args.isEnabled ?? true,
+    isEnabled: args.isEnabled ?? (isDev && !isProd),
     templates: args.templates,
     // Can't default this without conflicting with server/client seperation
     t: args.t,
