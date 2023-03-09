8c6e71c4cc5cc8c8127f2eaa9dbc344c3357f2f9
Nick Sinclair <nicksinclair@xtra.co.nz>
2023-03-10 10:06:36 +1300

    Update tsup

--- packages/server/tsup.config.ts
+++ packages/server/tsup.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "tsup";
+import pkgJson from "./package.json";
 
 export default defineConfig({
   entry: {
     index: "src/index.ts",
-    trpc: "src/trpc/index.ts",
-    sdk: "src/sdk/index.ts",
     "next-loader": "src/next/webpack/loader.ts",
-    next: "src/next/index.ts",
-    devtools: "src/devtools/index.ts",
-    util: "src/util/index.ts",
+    ...pkgJson.entrypoints.reduce((obj, entry) => {
+      obj[entry] = `src/${entry}.ts`;
+      return obj;
+    }, {}),
   },
   format: ["cjs", "esm"],
   bundle: true,
