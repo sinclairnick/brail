3b6da174aaa00b3accba89f79486116219d65cef
Nick Sinclair <nicksinclair@xtra.co.nz>
2023-03-13 16:47:36 +1300

    Add favicon

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -42,4 +42,37 @@ export default {
       </a>
     ),
   },
+  head: () => {
+    return (
+      <>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/images/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/images/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/images/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/images/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/images/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#2b5797" />
+        <meta name="msapplication-config" content="/images/browserconfig.xml" />
+        <meta name="theme-color" content="#ffffff" />
+      </>
+    );
+  },
 };
